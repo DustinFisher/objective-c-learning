ac0ad41be5ef112a72d71b47b6db8509a2d442f1
Dustin Fisher <dfisher77@gmail.com>
2014-01-05 10:24:04 -0500

    ch. 3

---
+++ BNR - Objective C Programming/ch. 3/TwoFloats/TwoFloats/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Dustin Fisher on 1/5/14.
+//  Copyright (c) 2014 Dustin Fisher. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    float a = 7.7;
+    float b = 14.2;
+    
+    double result = a + b;
+    
+    printf("The result is: %f\n", result);
+}
+
